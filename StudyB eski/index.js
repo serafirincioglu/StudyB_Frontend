fd9c70d8b05c739c7e5dfca7466a2d5015353e51
betimdogan <43788924+betimdogan@users.noreply.github.com>
2020-05-29 15:23:34 +0300

    seraya

---
+++ StudyB eski/index.js
@@ -0,0 +1,6 @@
+import {AppRegistry} from 'react-native';
+import App from './src/components/CoursePage';
+//import App from './App';
+
+
+AppRegistry.registerComponent('StudyB', () => App);
---
+++ StudyB eski/src/components/InputText.js
@@ -0,0 +1,91 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import {TextInput, Text, View, StyleSheet,Dimensions} from 'react-native';
+import { max } from 'react-native-reanimated';
+const {widht: WIDTH } = Dimensions.get('window')
+const propTypes = {
+    mapElement: PropTypes.func,
+    onSubmitEditing: PropTypes.func,
+    onChangeText: PropTypes.func,
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    maxLenght: PropTypes.number,
+    keyboardType: PropTypes.string,
+    secureTextEntry: PropTypes.bool,
+    label: PropTypes.string
+};
+
+const defaultProps = {
+    mapElement: (n) => {},
+    onSubmitEditing: () => {},
+    onChangeText: () => {},
+    value: "",
+    placeholder: "",
+    maxLenght: 200,
+    keyboardType: "default",
+    secureTextEntry: false,
+    label: ""
+
+};
+
+const styles = StyleSheet.create({
+    inputBox: {
+        width: WIDTH,
+        height: 45,
+        borderRadius: 25,
+        fontSize: 16,
+        paddingLeft: 45, 
+        backgroundColor: 'rgba(0, 0, 0, 1)',
+        color: 'rgba(255,255,255, 1)',
+        marginHorizontal: 0,
+        marginVertical: 15
+    }
+});
+
+class InputText extends Component{
+   state = {
+       value: ""
+   }
+   componentDidMount(){
+        this.setState({
+            value: this.props.value
+        })
+
+   }
+   onChangeText = (value) => {
+       
+    this.setState({
+            value
+       }, () => {
+           this.props.onChangeText(value);
+       }
+       )
+   }
+    
+    render() {
+        console.log(this.props.value);
+        const {placeholder,secureTextEntry,keyboardType,maxLenght,value,onChangeText,onSubmitEditing} = this.props;
+        return (
+            <View>
+                <TextInput
+                    style={styles.inputBox}
+                    placeholder={placeholder}
+                    placeholderTextColor="rgba(255,255,255,0.8)"
+                    selectionColor="#999999"
+                    secureTextEntry={secureTextEntry}
+                    keyboardType={keyboardType}
+                    maxLength={maxLenght}
+                    returnKeyType="next"
+                    value={this.state.value}
+                    onSubmitEditing={onSubmitEditing}
+                    onChangeText={this.onChangeText}/>
+            </View>
+        )
+    }
+
+
+}
+
+InputText.defaultProps = defaultProps;
+InputText.propTypes = propTypes;
+export default InputText;
