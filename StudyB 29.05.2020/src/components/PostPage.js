137e5a34154ef7b222e91907a04b784064092371
betimdogan <43788924+betimdogan@users.noreply.github.com>
2020-05-29 15:22:22 +0300

    sera

---
+++ StudyB 29.05.2020/src/components/PostPage.js
@@ -0,0 +1,346 @@
+import React, { useState,Component }  from 'react';
+import { StyleSheet,Text, View, TextInput, Button, ScrollView, FlatList, TouchableOpacity, Modal } from 'react-native';
+import {ListItem,CheckBox, ButtonGroup,Icon} from 'react-native-elements';
+
+import PostItem from './PostItem';
+import PostInput from './PostInput';
+import NavigationBar from './NavigationBar';
+import Book from '../images/CoursesIcon';
+import axios from 'axios';
+
+export default class PostPage extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {courseGoals: [],
+                   comment: '', 
+                   nameOfChatroom: '', 
+                   usernameOfChatroom: [],
+                  };
+    
+  }
+
+  addGoalHandler = (goalTitle) => {
+    
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      {id:goalTitle.id , value: goalTitle.text}
+    ]); 
+    setIsAddMode(false);
+  };
+
+  removeGoalHandler = goalId =>{
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal)=> goal.id !== goalId);
+    });
+  };
+
+  cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  } 
+  
+  // componentDidMount(){
+  //   const config = {
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //         'Accept': 'application/json'
+  //   }}
+  //   Promise.all([
+  //     axios.get('https://studyb.azurewebsites.net/api/chatrooms/' + onlineChatroom + '/messages/',config),
+  //     axios.get('https://studyb.azurewebsites.net/api/chatrooms/' + onlineChatroom ,config),
+  //     axios.post('https://studyb.azurewebsites.net/api/messages/' + onlineChatroom + '/users/' + onlineUser + '/messages/' , {text: this.state.comment}, config),
+
+  //   ]).then((response1, response2, response3) => {
+  //       //response.data.foreach(d => this.addGoalHandler(d))
+  //       //console.log("reponse of courses page" +   response.data);
+  //       this.setState({courseGoals: response1.data });
+  //       this.setState({chatroomName: response2.data.chatroomName});
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+
+  // }
+ //Calisan kod buuu
+  componentDidMount(){
+    const config = {
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+    }}
+
+  //   axios.get('https://studyb.azurewebsites.net/api/chatrooms/' + onlineChatroom + '/messages/',config)
+  //     .then(response => {
+  //       //response.data.foreach(d => this.addGoalHandler(d))
+  //       console.log("reponse of courses page" +   response.data);
+  //       this.setState({courseGoals: response.data });
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+  // //}
+//-------------------------------------------
+  // componentDidMount2(){
+    // const config = {
+    //   headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json'
+    // }}
+    Promise.all([
+      
+
+      axios.get('https://studyb.azurewebsites.net/api/chatrooms/'+ onlineChatroom + '/messages/', config),
+      axios.get('https://studyb.azurewebsites.net/api/chatrooms/' + onlineChatroom ,config),
+      axios.get('https://studyb.azurewebsites.net/api/chatrooms/' + onlineChatroom + '/messages/',config)
+    ])
+      .then(([response1, response2,response]) => {
+        //response.data.foreach(d => this.addGoalHandler(d))
+     
+        //this.setState({courseGoals: response.data });
+        // console.log("Before"+usernameOfChatroom);
+        // console.log("response1.data.userName : " + response1.data.userName);
+        // console.log("response1.userName : " + response1.userName);
+  
+        console.log("response1.data : " + response1.data);
+
+         this.setState({usernameOfChatroom: response1.data});
+        //  console.log("After" + usernameOfChatroom);
+        
+        this.setState({chatroomName: response2.data.chatroomName});
+        this.setState({courseGoals: response.data });
+
+      })
+      // .catch(function (error) {
+      //   console.log(error);
+      // })
+  }
+
+  handleTextChange = (inputText) => {
+    this.state.comment = inputText;
+  }
+
+  // getCourseName(){
+  //   const config = {
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //         'Accept': 'application/json'
+  //   }}
+    
+  //   axios.get('https://studyb.azurewebsites.net/api/chatrooms/' + onlineChatroom ,config)
+  //     .then(response => {
+  //       //response.data.foreach(d => this.addGoalHandler(d))
+        
+  //       this.setState({chatroomName: response.data.chatroomName});
+        
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+
+  //}
+
+  // getusername(){
+  //   const config = {
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //         'Accept': 'application/json'
+  //   }}
+    
+  //   axios.get('https://studyb.azurewebsites.net/api/users/' + onlineUser ,config)
+  //     .then(response => {
+  //       //response.data.foreach(d => this.addGoalHandler(d))
+  //       console.log("response.data.userName");
+  //       console.log(response.data.userName);
+  //       this.setState({usernameOfChatroom: response.data.userName});
+        
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+
+  // }
+
+
+  postSomething(){
+    console.log("this.state.comment!");
+    console.log(this.state.comment);
+   
+    const config = {
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+    }}
+
+    axios.post('https://studyb.azurewebsites.net/api/messages/' + onlineChatroom + '/users/' + onlineUser + '/messages/' , {text: this.state.comment}, config)
+      .then(response => {
+        console.log("Response : " +   response.id);
+        console.log("Response.text : " +   response.text);
+        this.componentDidMount();
+       
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log("Failed to post !");
+      })
+
+  }
+
+  
+
+  render(){
+    //this.getCourseName();
+    //this.getusername();
+    
+    return (
+      
+      <View style={styles.screen}>    
+        
+        <NavigationBar />
+        
+        <View style={styles.blueLine}>
+          <View style={styles.iconText}>
+            <Book />
+            <Text style={styles.profText}>{this.state.chatroomName}</Text>
+          </View>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}  
+            onChangeText={this.handleTextChange}  
+            placeholder="Enter your comment"  
+            placeholderTextColor='#56D6E0' 
+            
+          >
+          </TextInput>
+        </View>
+        
+        <TouchableOpacity style={styles.button} onPress={() => this.postSomething()}>
+          <Text style={styles.buttonText}>Post Something!</Text>
+        </TouchableOpacity>
+
+        <View style={styles.container}>
+          <Text style={styles.text}>Courses</Text>
+          
+          <ScrollView>
+          {   
+
+              this.state.courseGoals.map((l,i) => (
+                
+                    <ListItem style= {styles.listStyle}
+                        
+                        key={i}
+                        title={l.userName}
+                        titleStyle={{ color: 'black', fontSize: 20, fontWeight: 'bold', backgroundColor: 'white'}}
+                        subtitle={l.text}
+                        subtitleStyle={{ color: 'black' , backgroundColor: 'white'}}
+                        rightSubtitle={l.dateOfPost.slice(11,16)}
+                        color='black' 
+                        bottomDivider
+                
+
+                    />
+              
+                  )   
+              )  
+          }
+          </ScrollView>
+        </View>
+      </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+  screen: {
+    flex:1,
+    padding: 50,
+    backgroundColor: 'black',
+  },
+  
+  iconText:{
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  
+  blueLine: {
+    flexDirection: 'column',
+    backgroundColor: '#56D6E0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    height: '8%',
+    marginBottom: 20,
+    marginTop: 10
+  },
+  
+  profText: {
+    fontSize:18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginLeft: 10
+
+  },
+  listStyle: {
+    
+    paddingTop: 10,
+    paddingHorizontal:10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center', 
+    color: '#56D6E0',
+    flex:1
+
+},
+  button:{
+    backgroundColor:'#56D6E0',
+    width: 200,
+    height: 50,
+    borderRadius: 25,
+    paddingVertical: 16,
+    paddingHorizontal: 10,
+    alignSelf:'center',
+    marginBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  
+  buttonText: {
+    fontSize:16,
+    fontWeight: 'bold',
+    color:'#ffffff',
+    textAlign: 'center',
+    alignItems: 'center'
+
+  },
+  inputContainer: { 
+    padding: 10, 
+    marginBottom: 10,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  input: {
+    width: '80%',
+    borderColor: '#56D6E0',
+    borderWidth: 1,
+    padding: 10, 
+    marginBottom: 10,
+    color: '#56D6E0',
+    fontSize: 16,
+    borderRadius: 20
+},
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
+    backgroundColor: 'black'
+  }, 
+  
+});
---
+++ StudyB 29.05.2020/src/components/RewardPage.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+    Image,
+    Text,
+  } from 'react-native';
+  
+
+export default class RewardPage extends Component{
+    render(){
+        return(
+            <View style={styles.rewardscreen}>
+                
+                <View style={styles.NavigBar}>
+                    <View style={styles.studyBText}>
+                    <Image style={{width: 50, height:50}}
+                        source={require('../images/lastest_logo.png')}/>
+                    <Text style={styles.BarText}>StudyB</Text>
+                    </View>
+                </View>
+
+                <View style={styles.blueLine}>
+      
+                <View style={styles.iconText}>
+                <Image style={{width: 30, height:30}}
+                        source={require('../images/reward.png')}/>
+                <Text style={styles.profText} >Rewards</Text>
+                <Image style={{width: 30, height:30}}
+                        source={require('../images/reward.png')}/>
+                </View>
+
+                </View>
+
+                
+
+            </View>
+
+        )
+    }
+}
+  
+const styles = StyleSheet.create(
+    {
+        rewardscreen: {
+            backgroundColor: 'black',
+            flex: 1,
+        },
+        container: {
+            justifyContent:'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            backgroundColor: 'black'
+            
+
+        },
+
+        NavigBar: {
+            flexDirection: 'row',
+            justifyContent:'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            backgroundColor:'black',
+            marginBottom: 20,
+            width: '60%',
+            alignSelf:'center', 
+            paddingTop: 60,
+            
+           
+        },
+        iconText:{
+            flexDirection: 'row',
+            alignItems: 'center'
+          },
+          blueLine: {
+            flexDirection: 'column',
+            backgroundColor: '#56D6E0',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            height: '8%',
+            marginBottom: 20,
+            marginTop: 10
+          },
+          profText: {
+            fontSize:25,
+            fontWeight: 'bold',
+            color: '#ffffff',
+            textAlign: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+            marginLeft: 10,
+            marginRight: 10
+        
+          },
+        BarText: {
+            fontSize:16,
+            fontWeight: 'bold',
+            color: '#56D6E0',
+            textAlign: 'center',
+            alignItems: 'center'
+        
+          },
+          studyBText: {
+              flexDirection: 'row',
+              width: '40%',
+              justifyContent:'space-between',
+              alignItems: 'center',
+              alignSelf: 'center',
+          }
+    }
+)
