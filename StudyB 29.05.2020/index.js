2282d1f322b0b161015ab3a61e4e692d76c9b025
betimdogan <43788924+betimdogan@users.noreply.github.com>
2020-05-29 22:33:34 +0300

    Yeni versiyon

--- StudyB 29.05.2020/index.js
+++
@@ -1,6 +0,0 @@
-import {AppRegistry} from 'react-native';
-//import App from './src/components/FeedPage';
-import App from './App';
-
-
-AppRegistry.registerComponent('StudyB', () => App);
--- StudyB_sera/index.js
+++
@@ -1,6 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './src/components/CoursePage';
-//import App from './App';
-
-
-AppRegistry.registerComponent('StudyB', () => App);
